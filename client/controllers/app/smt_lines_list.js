9f832e059c634818b75c5e06ba43772d98d44a60
Gadi Israeli <gadi.israeli@flex.com>
2018-01-12 02:37:50 -0800

    [Studio] Modified screen "smt_lines_list"

--- client/controllers/app/smt_lines_list.js
+++ client/controllers/app/smt_lines_list.js
@@ -2,13 +2,4 @@ angular.module('app').controller('app_smt_lines_list', app_smt_lines_list);
 function app_smt_lines_list($scope, app) {
     'use strict';
     app.init($scope);
-
-    $scope.sleep = function ( sleepDuration ){
-        var now = new Date().getTime();
-        while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
-    };
-    $scope.lockRefresh = function () {
-        $scope.sleep(180);
-    };
-    
 }
