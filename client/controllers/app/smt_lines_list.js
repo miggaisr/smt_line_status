59df09ae50c53e806e9b503b832023b0984626a7
Gadi Israeli <gadi.israeli@flex.com>
2018-01-22 01:50:08 -0800

    [Studio] Modified screen "smt_lines_list"

--- client/controllers/app/smt_lines_list.js
+++ client/controllers/app/smt_lines_list.js
@@ -2,6 +2,6 @@ angular.module('app').controller('app_smt_lines_list', app_smt_lines_list);
 function app_smt_lines_list($scope, app,$timeout) {
     'use strict';
     app.init($scope, function(){
-        $timeout(app.call('smt_lines_list.stopRefresh'), 1000);
+        //$timeout(app.call('smt_lines_list.stopRefresh'), 1000);
     });
 }
