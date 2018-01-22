05c5867d3f0cab44e6df7a2f9ed45339cdc17fe9
Gadi Israeli <gadi.israeli@flex.com>
2018-01-22 05:13:08 -0800

    [Studio] Modified screen "login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -1,6 +1,10 @@
 angular.module('app').controller('app_login', app_login);
 function app_login($scope, app, $q) {
     'use strict';
+    $scope.data = {
+     "username": "2604",
+     "password": "$Flex2018",
+    };
     app.init($scope);
     if (!$scope.data) {
         $scope.data = {};
