b4cee38c1ba307cb11d9867f4dbb5de9036c2ded
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-03-21 02:30:54 -0700

    [Studio] Modified screen "smt_line_status"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -1,4 +1,16 @@
 angular.module('app').controller('app', app);
 function app($scope, app) {
     'use strict';
+        if (window.isPlatform && window.isPlatform.powWow() && window.isPlatform.iOS() && window.cordova && window.cordova.plugins) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+        }
+
+        if (window.StatusBar) {
+            StatusBar.hide();
+            document.body.classList.add('nostatusbar');
+        }
+
+        if (window.screen && window.screen.orientation) {
+            screen.orientation.lock('portrait');
+        }
 }
